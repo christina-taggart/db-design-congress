05df9616ca567c1e337c8633fee6f49115bf87eb
Darcey Lachtman & John Olmsted <pair+dslachtman.qsymmachus@devbootcamp.com>
2014-01-23 11:04:28 -0800

    Add create_congress_members migration

---
+++ source/ar-sunlight-legislators/db/migrate/20140123185207_create_congress_members.rb
@@ -0,0 +1,18 @@
+class CreateCongressMembers < ActiveRecord::Migration
+  def self.change
+    create_table :congress_members do |col|
+      col.string :title
+      col.string :first_name
+      col.string :middle_name
+      col.string :last_name
+      col.string :name_suffix
+      col.string :nickname
+      col.string :party
+      col.string :state
+      col.integer :district
+      col.binary :in_office
+      col.string :gender
+      col.date :birthdate
+    end
+  end
+end
