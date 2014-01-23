1a4c40700b727ca3b065ba0676bb2f47dad78b04
Elliot Chalfant & <pair+epchalfant.hzla00@devbootcamp.com>
2014-01-23 12:04:41 -0800

    Finished Release 2

---
+++ source/ar-sunlight-legislators/spec/legislator_queries.rb
@@ -0,0 +1,7 @@
+require_relative '../app/models/legislator'
+
+# Legislator.get_legislators("CA")
+# Legislator.get_legislator_gender_percentage("Female")
+# Legislator.get_legislator_count
+# Legislator.delete_inactives
+# Legislator.get_all
