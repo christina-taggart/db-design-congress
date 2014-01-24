bae78bb45c93a7ae1d7ea2ed514f02d0f19eb7ea
Christina Taggart & Kevin Yeh <pair+christina-taggart.kyeh@devbootcamp.com>
2014-01-23 17:24:52 -0800

    Release 1: Scrub and Import the Data

--- source/ar-sunlight-legislators/lib/sunlight_legislators_importer.rb
+++ source/ar-sunlight-legislators/lib/sunlight_legislators_importer.rb
@@ -1,15 +1,43 @@
 require 'csv'
 
 class SunlightLegislatorsImporter
-  def self.import(filename)
-    csv = CSV.new(File.open(filename), :headers => true)
-    csv.each do |row|
-      row.each do |field, value|
-        # TODO: begin
-        raise NotImplementedError, "TODO: figure out what to do with this row and do it!"
-        # TODO: end
-      end
+  def self.import(filename=File.dirname(__FILE__) + "/../db/data/legislators.csv")
+
+    state_abbr = ['AL','AK','AZ','AR','CA','CO','CT',
+                  'DE','FL','GA','HI','ID','IL','IN',
+                  'IA','KS','KY','LA','ME','MD','MA',
+                  'MI','MN','MS','MO','MT','NE','NV',
+                  'NH','NJ','NM','NY','NC','ND','OH',
+                  'OK','OR','PA','RI','SC','SD','TN',
+                  'TX','UT','VT','VA','WA','WV','WI',
+                  'WY']
+
+    party_abbr = ['D', 'R', 'I']
+
+    state_abbr.each do |state|
+      State.create(state_abbr: state)
     end
+
+    party_abbr.each do |party|
+      Party.create(party_name: party)
+    end
+
+    distinct_parties = Hash.new(0)
+
+    p filename
+    csv = CSV.new(File.open(filename), :headers => true)
+     first_line = true
+     CSV.foreach(File.open(filename)) do |line|
+        if first_line
+          first_line = false
+        else
+          new_politician =Politician.create(office_name: line[0], first_name: line[1], last_name: line[3],
+                             state: State.where('state_abbr = ?', line[7])[0], gender: line[10],
+                             party: Party.where('party_name = ?', line[6])[0] )
+          Contact.create(politician: new_politician, phone: line[11], fax: line[12] , website: line[13], webform: line[14], twitter_name: line[21])
+
+        end
+      end
   end
 end
 
