0b1a214efdf45fe94243d7dc5bc176cc207ede1b
Emily Foley & Ricardo A. Rubio <pair+EmFoley.rickarubio@devbootcamp.com>
2014-01-23 18:07:25 -0800

    Release 1, data scrubbed and imported

--- source/ar-sunlight-legislators/db/migrate/20140123183242_create_legislators.rb
+++ source/ar-sunlight-legislators/db/migrate/20140123183242_create_legislators.rb
@@ -3,12 +3,19 @@
 class CreateLegislators < ActiveRecord::Migration
   def change
     create_table :legislators do |t|
-      t.string :title, :party, :state, :name, :phone, :fax, :website, :email,
+      t.string :title, :name, :party, :state, :phone, :fax, :website,
                :gender, :twitter_id
-      t.date :birthday
+      t.date :birthdate
       t.boolean :in_office
-
+      # t.string :fec_id
       t.timestamps
     end
   end
 end
+
+
+# "title","firstname","middlename","lastname","name_suffix","nickname",
+# "party","state","district","in_office","gender","phone","fax","website",
+# "webform","congress_office","bioguide_id","votesmart_id","fec_id","govtrack_id",
+# "crp_id","twitter_id","congresspedia_url","youtube_url","facebook_id","official_rss",
+# "senate_class","birthdate"
